7da78f17ee1b0b44aa38f586eb1778d97d03730d
Sehyeon Kim <sue2368@gmail.com>
2023-05-27 00:02:21 +0900

    add video

--- uploader/urls.py
+++ uploader/urls.py
@@ -1,5 +1,5 @@
 from rest_framework.routers import SimpleRouter
 from .views import VideoViewset
 router = SimpleRouter()
-router.register('accounts', VideoViewset)
+router.register('video', VideoViewset)
 urlpatterns = router.urls
