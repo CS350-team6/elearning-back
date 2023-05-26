8743063e9161d9d552ca8f89019ebe97bb2b968b
Happy-hemoglobin <hyeonbhin.byeon@kaist.ac.kr>
2023-05-27 03:55:35 +0900

    Block signup with same ID. Impl pwchange, withdraw account

--- user_account/urls.py
+++ user_account/urls.py
@@ -3,10 +3,12 @@ from . import views
 
 app_name = "user_account"
 urlpatterns = [
+    path("", views.withdraw_account , name="withdraw_account"),
     path("home/", views.home , name="home"),
     path("signup/", views.signup, name="signup"),
     path("login/", views.login, name="login"),
     path("logout/", views.logout, name="logout"),
+    path("pwchange/", views.pwchange, name="pwchange"),
     #path("<int:question_id>/", views.detail, name="detail"),
     #path("<int:question_id>/results/", views.results, name="results"),
     #path("<int:question_id>/vote/", views.vote, name="vote"),
--- user_account/views.py
+++ user_account/views.py
@@ -39,39 +39,89 @@ def generate_jwt_token(user, user_id):
 def signup(request):
     if request.method == 'POST':
         content= json.loads(request.body)
-        user = User.objects.create_user(
-            username = content['email'],
-            password = content['password'],
+        try:
+            user = User.objects.create_user(
+            username = content['userId'],
+            password = content['userPw'],
         )
-        new_jwt_token = generate_jwt_token(user, content['email'])
+        except:
+            return HttpResponse(json.dumps({'result': "false", "jwt": "Invaild"})) # Check if the user already exists
+        new_jwt_token = generate_jwt_token(user, content['userId'])
         extended_user = UserInfo.objects.create(
             user = user,
             jwt_token = new_jwt_token,
             nickname = "test nickname"
         )
+        token_string = str(new_jwt_token.token).split("'")[1]
         auth.login(request, user)
-        return HttpResponse(json.dumps({'result': "true", 'nickname': extended_user.nickname}))
-    return HttpResponse(json.dumps({'result': "false"}))
+        return HttpResponse(json.dumps({'result': "true", "jwt": token_string}))
+    return HttpResponse(json.dumps({'result': "false", "jwt": "Invaild"}))
 
 @method_decorator(csrf_exempt, name="dispatch")
 def login(request):
     if request.method == 'POST':
         content= json.loads(request.body)
-        username = content['email']
-        password = content['password']
+        username = content['userId']
+        password = content['userPw']
         user = authenticate(request, username=username, password=password)
         if user is not None:
             auth.login(request, user)
-            extended_user = UserInfo.objects.get(user_id_id=user)
-            return HttpResponse(json.dumps({'result': "true", 'nickname': extended_user.nickname}))
+            extended_user = UserInfo.objects.get(user_id=user)
+            token_string = str(extended_user.jwt_token.token).split("'")[1]
+            return HttpResponse(json.dumps({'result': "true", "jwt": token_string}))
+        else:
+            return HttpResponse(json.dumps({'result': "false", "jwt": "Invaild"}))
+    else:
+        return HttpResponse(json.dumps({'result': "false", "jwt": "Invaild"}))
+
+@method_decorator(csrf_exempt, name="dispatch")
+def logout(request):
+    if request.method == 'PUT':
+        auth.logout(request)
+        return HttpResponse(json.dumps({'result': "true"}))
+    else:
+        return HttpResponse(json.dumps({'result': "false"}))
+
+@method_decorator(csrf_exempt, name="dispatch")
+def withdraw_account(request):
+    if request.method == 'DELETE':
+        content= json.loads(request.body)
+        token= content['jwt'].encode('utf-8')
+        payload = jwt.decode(token, settings.SECRET_KEY, algorithms=["HS256"])
+        try:
+            user = User.objects.get(username=payload["user_id"])
+        except:
+            return HttpResponse(json.dumps({'result': "false"}))
+        
+        if user is not None:
+            extended_user = UserInfo.objects.get(user_id=user)
+            extended_user.jwt_token.delete()
+            extended_user.user.delete()
+            extended_user.delete()
+            return HttpResponse(json.dumps({'result': "true"}))
         else:
             return HttpResponse(json.dumps({'result': "false"}))
     else:
         return HttpResponse(json.dumps({'result': "false"}))
 
-def logout(request):
-    auth.logout(request)
-    return HttpResponse(json.dumps({'result': "true"}))
-
+@method_decorator(csrf_exempt, name="dispatch")
 def home(request):
     return HttpResponse(json.dumps({'result': "true"}))
+
+@method_decorator(csrf_exempt, name="dispatch")
+def pwchange(request):
+    if request.method == 'PUT':
+        content= json.loads(request.body)
+        token= content['jwt'].encode('utf-8')
+        payload = jwt.decode(token, settings.SECRET_KEY, algorithms=["HS256"])
+        user = authenticate(request, username=payload["user_id"], password=content["userPw"])
+        if user is not None:
+            user.set_password(content["newPw"])
+            user.save()
+            return HttpResponse(json.dumps({'result': "true"}))
+        else:
+            return HttpResponse(json.dumps({'result': "false"}))
+    else:
+        return HttpResponse(json.dumps({'result': "false"}))
+    
+
