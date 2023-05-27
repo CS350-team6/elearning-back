cce65175d4117cc07777007cf1aaab40f5c1fca6
Sehyeon Kim <sue2368@gmail.com>
2023-05-27 15:28:12 +0900

    resolve model no default value error

--- user_account/models.py
+++ user_account/models.py
@@ -11,22 +11,3 @@ class UserInfo(models.Model):
 
     #def was_published_recently(self):
     #    return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
-
-class Student(models.Model):
-    user = models.OneToOneField(UserInfo, on_delete=models.CASCADE)
-    # Add additional fields specific to the Student table
-    student_id = models.CharField(max_length=20)
-    major = models.CharField(max_length=100)
-
-    def __str__(self):
-        return self.user.nickname
-
-
-class Instructor(models.Model):
-    user = models.OneToOneField(UserInfo, on_delete=models.CASCADE)
-    # Add additional fields specific to the Instructor table
-    instructor_id = models.CharField(max_length=20)
-    department = models.CharField(max_length=100)
-
-    def __str__(self):
-        return self.user.nickname
