fbad2a8101f2d758cb6846226d6b6913e66951d2
Sehyeon Kim <sue2368@gmail.com>
2023-06-09 00:06:57 +0900

    integration for applying to main code

--- elearning/urls.py
+++ elearning/urls.py
@@ -8,14 +8,11 @@ from rest_framework_simplejwt.views import TokenRefreshView
 from rest_framework.routers import SimpleRouter
 
 # App imports
-from .views import user_view, myuser_view, lecture_view
+from .views import user_view, lecture_view
 
 router = SimpleRouter()
 router.register('users', user_view.UserViewSet)
 urlpatterns = [
-    # path('token-auth/', myuser_view.CustomTokenObtainPairView.as_view()),
-    # path('token-refresh/', TokenRefreshView.as_view()),
-    # path('myusers', myuser_view.User.as_view()),
     path('lectures', lecture_view.Lecture.as_view()),
     path('lectures/<int:pk>', lecture_view.LectureDetail.as_view()),
 ]
