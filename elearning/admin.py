d2dc9804a02aa49732d4b3cfbc0f442404a68858
Sehyeon Kim <sue2368@gmail.com>
2023-06-09 10:12:05 +0900

    remove unused user_model import

--- elearning/admin.py
+++ elearning/admin.py
@@ -1,9 +1,7 @@
 from django.contrib import admin
-from .models import user_model, user_info_model, lecture_model
+from .models import user_info_model, lecture_model
 from django.contrib.auth.models import User
 
-# admin.site.register(User)
-# admin.site.register(user_model.User)
 admin.site.register(user_info_model.JWTToken)
 admin.site.register(lecture_model.Video)
 admin.site.register(lecture_model.Lecture)
--- elearning/models/user_info_model.py
+++ elearning/models/user_info_model.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.utils import timezone
 from datetime import timedelta
-from ..models import user_model, lecture_model
 from django.contrib.auth.models import User
 from .lecture_model import Lecture, Video
 
