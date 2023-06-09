0526d7e553347624c7f9378e910875f7ebbc9a3d
Sehyeon Kim <sue2368@gmail.com>
2023-06-09 10:23:02 +0900

    integrate to one url file

--- conf/urls.py
+++ conf/urls.py
@@ -1,35 +1,27 @@
-"""
-URL configuration for conf project.
+# Standard Library imports
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
+# Core Django imports
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+
+# Third-party imports
+from rest_framework.routers import SimpleRouter
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
-from rest_framework.schemas import get_schema_view
-from rest_framework.renderers import JSONOpenAPIRenderer
 
-schema_view = get_schema_view(title="Example API", renderer_classes=[JSONOpenAPIRenderer])
+# App imports
+from elearning.views import user_view, lecture_view
 
-urlpatterns = [
+router = SimpleRouter()
+router.register('users', user_view.UserViewSet)
+urlpatterns = router.urls
+urlpatterns += [
     path("admin/", admin.site.urls),
-    path('schema', schema_view),
-    path("api/", include("elearning.urls")),
     path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
     path("api/schema/docs/", SpectacularSwaggerView.as_view(url_name="schema"), name="docs"),
+    path('lectures', lecture_view.Lecture.as_view()),
+    path('lectures/<int:pk>', lecture_view.LectureDetail.as_view()),
 ]
 
 if settings.DEBUG:
